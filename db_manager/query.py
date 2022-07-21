91bb3c5cad52dfc721f79734872e35185c9395b8
Naveen Singh <naveensingh@Naveens-MacBook-Air.local>
2022-07-21 18:11:32 +0530

    added tasks for db

---
+++ db_manager/query.py
@@ -0,0 +1,21 @@
+"""
+Write query to create a table to database to store below information
+id
+name
+email
+phone_number
+skills
+file_path
+"""
+
+"""
+Write SQL query to Insert data in the above table
+"""
+
+"""
+Write SQL query to Fetch data from the above table
+"""
+
+"""
+Write SQL query to delete data from the above table based on the ID
+"""
