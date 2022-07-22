8a16dea6c6d0618b96cf7acc828b121a6248de0b
Priyanshi <priyanshisrivatava187@gmail.com>
2022-07-22 18:06:09 +0530

    Created user function and created save data function

--- cv_manager/main.py
+++ cv_manager/main.py
@@ -1,11 +1,22 @@
+import sqlite3
 import os
 from cv_manager.process_doc import get_data_from_doc
+from db_manager.query import CREATE_TABLE_QUERY
 UPLOAD_DIR = os.path.join(os.getcwd(), "uploads")
+
+
+
 
 def save_data(data):
     """
     Save the data in DB using the db functions
     """
+    conn = sqlite3.connect(<database_name>)
+    cursor = conn.cursor()
+    cursor.execute(CREATE_TABLE_QUERY)
+    conn.commit()
+    conn.close()
+    print("Save Detail")
     pass
 
 def extract_data_from_file():
