a2a46a808ef4bbbac6b146577bbb1844672ce747
Lopamudra99 <sahoo.lopa99@gmail.com>
2022-07-23 12:09:43 +0530

    lopa pr

--- db_manager/fetch_profile.py
+++ db_manager/fetch_profile.py
@@ -1,7 +1,13 @@
+import sqlite3
+
+
 def fetch_user():
     """
     Create a function to fetch users fro DB
 
     Created By: lopamudra.sahoo@ag-technologies.com
     """
+
+    conn = sqlite3.connect("")
+    conn.close()
     pass
