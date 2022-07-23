d60cd8e74c89d80ed1e749da6dc4ad2aeb9a0aa1
Niru-baby <nirupamasahoo757@gmail.com>
2022-07-23 10:33:11 +0530

    create function completed

--- db_manager/create_table.py
+++ db_manager/create_table.py
@@ -8,10 +8,13 @@ def create_table():
     """
     Create a function to create table using the create table query
 
-    Created By: nirupama.sahoo@ag-technologies.com
+    Created By: nirupama.sahoo@ag-technologies.com 
     """
+
+
     conn = sqlite3.connect("new_table.db")
     cursor = conn.cursor()
     cursor.execute(CREATE_TABLE_QS)
     conn.close()
     print("Created Employee Table")
+    pass
